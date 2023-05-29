8e6571437d7a12b79176b5ec4b7650b2c1af9ca6
marius <marius1890@yahoo.com>
2023-05-29 20:26:43 +0300

    update

--- main.py
+++ main.py
@@ -12,12 +12,14 @@ def localizare():
     apus: int = apus_user()
 
     while True:
-        if (iss_lat - USER_LAT > 0 or iss_lat - USER_LAT <= 5) and (
-                iss_lng - USER_LNG > 0 or iss_lng - USER_LNG <= 5) and ((ora_user >= apus) or (ora_user < rasarit)):
-            send_email(ora_adevar=ora_user)
-            break
         print(f'lat_iss: {iss_lat}, lng_iss: {iss_lng}, ora mea: {ora_user}, rasarit: {rasarit}, apus: {apus}')
         print(f"Latitudine apropiere: {round(iss_lat - USER_LAT, 2)}, Longitudine apropiere: {round(iss_lng - USER_LNG, 2)}")
+        if (iss_lat - USER_LAT > 0 and iss_lat - USER_LAT <= 5) and (
+                iss_lng - USER_LNG > 0 and iss_lng - USER_LNG <= 5) and ((ora_user >= apus) or (ora_user < rasarit)):
+            # send_email(ora_adevar=ora_user)
+            print('am intrat')
+            break
+
         iss_lat: float = round(lat_iss(), 2)
         iss_lng: float = round(lng_iss(), 2)
         ora_user: int = timp_user()['ora']
